84aead4ba6ca35be6eefd304184af934e2fcc288
beattyoc <beattyoc@tcd.ie>
2015-10-26 17:54:49 +0000

    updates made after testing server's execution

--- lab2/lab2_server.rb
+++ lab2/lab2_server.rb
@@ -1,12 +1,14 @@
 require 'socket'                    # Get sockets from stdlib
 require 'thread'                    # stdlib
+require 'open-uri'
 
 class Server
   def initialize()
     @port = ARGV[0]                     # portnumber parameter
     @hostname = '0.0.0.0'
     @server = TCPServer.open(@hostname, @port)     # open socket
-    @ipaddress = Socket.ip_address_list.find { |ai| ai.ipv4? && !ai.ipv4_loopback? }.ip_address #get IPAddress
+    #@ipaddress = Socket.ip_address_list.find { |ai| ai.ipv4? && !ai.ipv4_loopback? }.ip_address #get IPAddress
+    @ipaddress = open('http://whatismyip.akamai.com').read
     @workQ = Queue.new
     @pool_size = 10                      # for example there may be a max number of 5 threads
     puts "Current pool size is #{@pool_size}"
@@ -18,19 +20,21 @@ class Server
     loop do
       if @workQ.size < (@pool_size-1)
         Thread.start(@server.accept) do |client|
-        client.puts('Connection made')
+        #client.puts('Connection made')
         @workQ.push 1
         puts "#{@workQ.size} threads in use"
         line = client.gets
         if line.include?('HELO')          # if client writes HELO text\n
-          client.puts line + "IP:[#{@ipaddress}]\nPort:[#{@port}]\nStudentID:[66a55996468091b1f6f3b52e3181ccbcc584d5134ccb47e80c0797fed3ca9545]\n"
+          client.puts line + "IP:#{@ipaddress}\nPort:#{@port}\nStudentID:[66a55996468091b1f6f3b52e3181ccbcc584d5134ccb47e80c0797fed3ca9545]\n"
+          client.close
         elsif line.include?('KILL_SERVICE')  # if client writes KILL_SERVICE
-          client.puts 'Exiting service...'
+          #client.puts 'Exiting service...'
+          client.close
           Kernel.exit
         else
           puts 'Thread going to sleep for 5s'
           sleep 5                          # to simulate thread in use
-          client.puts('Disconnecting')
+          #client.puts('Disconnecting')
           client.close                       # disconnect from client
         end
         @workQ.pop(true)
