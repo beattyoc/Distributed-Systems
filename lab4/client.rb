054f11ffa83a363db916e156099af758eccf9e15
Claire <beattyoc@tcd.ie>
2015-12-17 02:23:02 +0000

    incomplete

--- lab4/client.rb
+++ lab4/client.rb
@@ -13,13 +13,23 @@ class Client
 		listen
 		@request.join
 		@response.join
+		@client_name = "client1"
+		@join_id = ""
+		@room_ref = ""
+		@server_ip = ""
+		@port_num = ""
+		@chatroom_name = ""
 	end
 
 	def listen
 		@response = Thread.new do
 			loop {
-				msg = @server.gets.chomp
+				msg = @server.gets
 				puts "#{msg}"
+				#if msg.include?('JOINED_CHATROOM')
+				#	get_join_info(msg)
+				#else
+				#end
 			}
 		end
 	end
@@ -28,12 +38,30 @@ class Client
 		#puts 'Enter kill, join, leave or disconnect'
 		@request = Thread.new do
 			loop {
-				msg = "JOIN_CHATROOM: chat001\nCLIENT_IP: 0\nPORT: 0\nCLIENT_NAME: user1\n"
+				msg = "JOIN_CHATROOM:room1\nCLIENT_IP:0\nPORT:0\nCLIENT_NAME:#{@client_name}\n"
 				@server.puts(msg) 
+				#sleep 3
+				#msg = "LEAVE_CHATROOM:#{@room_ref}\nJOIN_ID:#{@join_id}\nCLIENT_NAME:#{@client_name}"
+				#@server.puts(msg)
 			}
 		end
 	end
+
+	def get_join_info(msg)
+		@chatroom_name = msg[/JOINED_CHATROOM:(.*)$/, 1]
+		msg = client.gets
+		@server_ip = msg[/SERVER_IP:(.*)$/, 1]
+		msg = client.gets
+		@port_num = msg[/PORT:(.*)$/, 1]
+		msg = client.gets
+		@room_ref = msg[/ROOM_REF:(.*)$/, 1]
+		msg = client.gets
+		@join_id = msg[/JOIN_ID:(.*)$/, 1]
+	end
+
+
 end
+
 
 server = TCPSocket.open("localhost", 8000)
 Client.new(server)
--- lab4/server.rb
+++ lab4/server.rb
@@ -2,22 +2,22 @@
 require 'socket'
 require 'thread'
 require 'open-uri'
+require 'securerandom'
 
 class Server
   def initialize()
     @port = ARGV[0]
-    #@hostname = '0.0.0.0'
-    @hostname = 'localhost'
+    @hostname = '0.0.0.0'
+    #@hostname = 'localhost'
     @server = TCPServer.open( @hostname, @port )
     @ipaddress = open('http://whatismyip.akamai.com').read
     @workQ = Queue.new
     @pool_size = 10
-    @connections = Hash.new
     @rooms = Hash.new
+    @rooms_by_id = Hash.new
     @clients = Hash.new
-    @connections[:server] = @server
-    @connections[:rooms] = @rooms
-    @connections[:clients] = @clients
+    @clients_by_id = Hash.new
+    @memberships = Hash.new
     puts "Current pool size is #{@pool_size}"
     puts "Listening on: #{@hostname}:#{@port}"
     run
@@ -28,7 +28,8 @@ class Server
       if @workQ.size < (@pool_size-1)
         Thread.start(@server.accept) do | client |
           @workQ.push 1
-          msg = client.gets.chomp
+          msg = client.gets
+          #puts "#{msg}"
           if msg.include?('KILL_SERVICE')  # if client writes KILL_SERVICE
             puts 'KILL REQUEST'
             client.close
@@ -70,21 +71,39 @@ class Server
   end
 
   def listen_user_messages( username, client )
-    puts "listen_user_messages"
-    loop do
-      msg = client.gets.chomp
-      puts "#{username}: #{msg}"
-      @connections[:clients].each do |other_name, other_client|
-        unless other_name == username
-          other_client.puts "#{username.to_s}: #{msg}"
-        end
-      end
+      loop do
+          msg = client.gets
+          #puts "#{username}: #{msg}"
+          if msg.include?('KILL_SERVICE')  # if client writes KILL_SERVIC
+            puts 'KILL REQUEST'
+            client.close
+            @server.close
+            Kernel.exit
+          elsif msg.include?('LEAVE_CHATROOM')
+            puts 'LEAVE REQUEST'
+            leave_request(msg, client)
+          elsif msg.include?('DISCONNECT')
+            puts 'DISCONNECT'
+            disconnect_request(msg, client)
+          elsif msg.include?('JOIN_CHATROOM')
+            puts 'JOIN REQUEST'
+            join_request(msg, client)
+          else
+          end
+      #@connections[:clients].each do |other_name, other_client|
+       # unless other_name == username
+        #  other_client.puts "#{username.to_s}: #{msg}"
+        #end
+      #end
     end
   end
 
+
+
+
+
   def join_request(msg, client)
-    # Get arguments
-    #puts "#{msg}"
+      
     chatroom_name = msg[/JOIN_CHATROOM:(.*)$/, 1]
     msg = client.gets
     client_ip = msg[/CLIENT_IP:(.*)/,1]
@@ -92,28 +111,98 @@ class Server
     port_num = msg[/PORT:(.*)/,1]
     msg = client.gets
     client_name = msg[/CLIENT_NAME:(.*)$/, 1]
-    join_id = SecureRandom.random_number(100)
-    puts join_id
-    #room.has_key?(chatroom_name)
-    @clients = client_name
-    @rooms = chatroom_name
-    @connections[:rooms] = chatroom_name
-    @connections[:clients] = client_name
-    client.puts("JOINED_CHATROOM:#{chatroom_name}\nSERVER_IP:#{@ipaddress}\nPORT:#{port_num}\nROOM_REF:#{123}\nJOIN_ID:#{321}\n")
-    puts "#{client_name} requests to join #{chatroom_name}"
+
+    join_id = ""
+    if @clients[client_name]
+      puts 'Repeat client'
+      join_id = @clients[client_name]
+      @clients_by_id[join_id] = client
+    else
+      join_id = SecureRandom.uuid.gsub("-", "").hex
+      @clients[client_name] = join_id
+      @clients_by_id[join_id] = client
+    end
+
+    room_ref = ""
+    if @rooms[chatroom_name]
+      puts 'Repeat room'
+      room_ref = @rooms[chatroom_name]
+      @rooms_by_id[room_ref] = chatroom_name
+    else
+      #room_ref = SecureRandom.uuid.gsub("-", "").hex
+      room_ref = SecureRandom.random_number(1000)
+      puts room_ref
+      @rooms[chatroom_name] = room_ref
+      @rooms_by_id[room_ref] = chatroom_name
+    end
+ 
+    @memberships[room_ref] = join_id
+    
+    client.puts("JOINED_CHATROOM:#{chatroom_name}\nSERVER_IP:#{@ipaddress}\nPORT:#{port_num}\nROOM_REF:#{room_ref}\nJOIN_ID:#{join_id}\n")
+    #client.puts("CHAT:#{room_ref}\nCLIENT_NAME:#{client_name}\nMESSAGE:#{client_name} has joined this chatroom.\n\n")
+    message_room("CHAT:#{room_ref}\nCLIENT_NAME:#{client_name}\nMESSAGE:#{client_name} has joined this chatroom.\n\n", room_ref)
+    listen_user_messages(client_name,client)
   end
+  
+  
+  
+  
 
   def leave_request(msg, client)
+      
+    room_ref = msg[/LEAVE_CHATROOM:(.*)$/, 1]
+    #puts room_ref
+    msg = client.gets
+    join_id = msg[/JOIN_ID:(.*)$/, 1]
+    msg = client.gets
+    client_name = msg[/CLIENT_NAME:(.*)$/, 1]
 
+    #leave
+    #puts @rooms_by_id
+
+    client.puts("LEFT_CHATROOM:#{room_ref}\nJOIN_ID:#{join_id}\n")
+    client.puts("CHAT:#{room_ref}\nCLIENT_NAME:#{client_name}\nMESSAGE:#{client_name} has left this chatroom.\n\n")
+    #message_room("CHAT:#{room_ref}\nCLIENT_NAME:#{client_name}\nMESSAGE:#{client_name} has left this chatroom.\n\n", room_ref)
+    listen_user_messages(client_name,client)
   end
+
+
 
   def chat_request(msg, client)
 
   end
 
-  def disconnect_request(msg, client)
 
+
+
+
+  def disconnect_request(msg, client)
+      client_ip = msg[/DISCONNECT:(.*)$/, 1]
+      msg = client.gets
+      port_num = msg[/PORT:(.*)$/, 1]
+      msg = client.gets
+      client_name = msg[/CLIENT_NAME:(.*)$/, 1]
+      client.close
   end
+
+
+
+
+
+  def message_room(msg,room_ref)
+    @memberships.each do |rr, ji|
+        #if(rr.eql?(room_ref))
+        if(rr == room_ref)
+            puts "rr matches"
+            @clients_by_id[ji].puts msg
+        else
+            puts "no match"
+        end
+    end
+  end
+  
+  
+  
 
 end
 
